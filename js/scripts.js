b4686d7aa7ec8c821bc9caf8395152889440d43e
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-15 09:15:53 -0800

    evalaute quiz by assigning number values to each answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,39 @@
-// $(document).ready(function() {
-//   $("form#quiz").submit(function(event) {
-//
-//     var dinner = $('input[name=optionsRadios1]:checked').val();
-//     var home = $('input[name=optionsRadios2]:checked').val();
-//     var self = $('input[name=optionsRadios3]:checked').val();
-//
-//     var questions = [dinner, home, self];
-//
-//     var cat = 0;
-//     var witch = 0;
-//     var mermaid = 0;
-//
-//     questions.forEach(function(question) {
-//       if (question === 'a1') {
-//         cat++;
-//       } else if (question === 'a2') {
-//         witch++;
-//       } else if (question === 'a3') {
-//         mermaid++;
-//       } else {
-//         alert("Please answer all questions")
-//       }
-//     });
-//
-//     if ((cat > witch) && (cat > mermaid)) {
-//       alert('Cat');
-//     } else if ((witch > cat) && (witch > mermaid)) {
-//       alert('Witch');
-//     } else {
-//       alert('Mermaid');
-//     }
-//
-//     event.preventDefault();
-//   });
-//
-// });
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+
+    var climate = $('input[name=climate]:checked').val();
+    var activity = $('input[name=activity]:checked').val();
+    var beauty = $('input[name=beauty]:checked').val();
+    var price = $('input[name=price]:checked').val();
+    var friends = $('input[name=friends]:checked').val();
+
+    var questions = [climate, activity, beauty, price, friends];
+
+    var iceland = 0;
+    var thailand = 0;
+    var whistler = 0;
+
+    questions.forEach(function(question) {
+      if (question === 'iceland') {
+        iceland++;
+      } else if (question === 'thailand') {
+        thailand++;
+      } else if (question === 'whistler') {
+        thailand++;
+      }
+    });
+
+    $('.destinations').children().hide();
+
+    if ((iceland > thailand) && (iceland > whistler)) {
+      $('#iceland').show();
+    } else if ((thailand > iceland) && (thailand > whistler)) {
+      $('#thailand').show();
+    } else {
+      $('#whistler').show();
+    }
+
+    event.preventDefault();
+  });
+
+});
