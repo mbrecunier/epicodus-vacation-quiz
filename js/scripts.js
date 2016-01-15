c7bd66bad472aeef13d810730d20e8b7b2ef60e2
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-15 10:15:22 -0800

    create functions for validation, evaluating quiz answers, and displaying results

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
 
+//create an array with the values of each question
     var climate = $('input[name=climate]:checked').val();
     var activity = $('input[name=activity]:checked').val();
     var beauty = $('input[name=beauty]:checked').val();
@@ -9,29 +10,58 @@ $(document).ready(function() {
 
     var questions = [climate, activity, beauty, price, friends];
 
+//create variables for each outcome that will be evaluated in a number system
     var iceland = 0;
     var thailand = 0;
     var whistler = 0;
 
-    questions.forEach(function(question) {
-      if (question === 'iceland') {
-        iceland++;
-      } else if (question === 'thailand') {
-        thailand++;
-      } else if (question === 'whistler') {
-        thailand++;
+//create functions to validate quiz, run quiz, and determine/show result
+    var radioButtons = $('.form-group');
+
+    var quizValidation = function() {
+      if (radioButtons.find('input:radio:checked').length === radioButtons.length) {
+        $('.error').remove();
+        return true;
+      } else {
+        return false;
       }
-    });
+    };
 
-    $('.destinations').children().hide();
+    var runQuiz = function() {
+      questions.forEach(function(question) {
+        if (question === 'iceland') {
+          iceland++;
+        } else if (question === 'thailand') {
+          thailand++;
+        } else {
+          whistler++;
+        }
+        resultReturn();
+      })
+    };
 
-    if ((iceland > thailand) && (iceland > whistler)) {
-      $('#iceland').show();
-    } else if ((thailand > iceland) && (thailand > whistler)) {
-      $('#thailand').show();
+    var resultReturn = function() {
+      $('.destinations').children().hide();
+      if ((iceland > thailand) && (iceland > whistler)) {
+        $('#iceland').show();
+      } else if ((thailand > iceland) && (thailand > whistler)) {
+        $('#thailand').show();
+      } else {
+        $('#whistler').show();
+      }
+    };
+
+
+//if all questions have been answer- run quiz, if a question is blank- show error
+    quizValidation();
+
+    if (quizValidation()) {
+      runQuiz();
     } else {
-      $('#whistler').show();
+      $('form#quiz').prepend('<h4 class="error">Oops! Please answer all of the questions</h4>')
+      return false;
     }
+
 
     event.preventDefault();
   });
